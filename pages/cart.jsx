1899ff97f0436226d0dfb2767e67cf63113fbaab
Fury <ashiqasraf07@gmail.com>
2022-02-15 21:03:24 +0530

    Completed responsive cart page

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -6,7 +6,7 @@ const Cart = () => {
 		<div className={styles.container}>
 			<div className={styles.left}>
 				<table className={styles.table}>
-					<tr className={styles.tr}>
+					<tr className={styles.trTitle}>
 						<th>Product</th>
 						<th>Name</th>
 						<th>Extras</th>
