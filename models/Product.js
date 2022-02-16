4a2b7087dfdccc119a18bd29ddc3898d44aea379
Fury <ashiqasraf07@gmail.com>
2022-02-16 11:27:57 +0530

    Completed models

---
+++ models/Product.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const ProductSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			maxlength: 60,
+		},
+		desc: {
+			type: String,
+			required: true,
+			maxlength: 200,
+		},
+		img: {
+			type: String,
+			required: true,
+		},
+		prices: {
+			type: [Number],
+			required: true,
+		},
+		extraOptions: {
+			type: [
+				{
+					text: {
+						type: String,
+						required: true,
+					},
+					price: {
+						type: Number,
+						required: true,
+					},
+				},
+			],
+			required: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema)
