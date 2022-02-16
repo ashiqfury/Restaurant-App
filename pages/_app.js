4b1cf207f4ab5e73f670af0a7ab715fa58e44fa4
Fury <ashiqasraf07@gmail.com>
2022-02-16 13:37:08 +0530

    Setup redux for cart page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.scss'
 import Layout from '../components/Layout'
+import store from 'redux/store'
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<Provider store={store}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</Provider>
 	)
 }
 
